b09caa67797982c5223c38eaaa8151a73cdbd108
Pascalduc <78885790+Pascalduc@users.noreply.github.com>
2021-06-05 21:54:25 -0400

    d1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,22 +10,3 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # summary of linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
 
-
-
-# confirm linear regression with visualization
-# check vehicle_length ***
-#create linear model
-plt <- ggplot(mechacar_mpg,aes(x=mpg,y=vehicle_length))
-plt + geom_point() 
-# check vehicle_length p-value 0.078
-plt <- ggplot(mechacar_mpg,aes(x=mpg,y=vehicle_weight))
-plt + geom_point()
-# check spoiler_angle p-value 0.3
-plt <- ggplot(mechacar_mpg,aes(x=mpg,y=spoiler_angle))
-plt + geom_point()
-# check ground_clearance p-value ***
-plt <- ggplot(mechacar_mpg,aes(x=mpg,y=ground_clearance))
-plt + geom_point()
-# check AWD p-value 0.19
-plt <- ggplot(mechacar_mpg,aes(x=mpg,y=AWD))
-plt + geom_point()
